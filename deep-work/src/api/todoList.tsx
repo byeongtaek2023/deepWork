7170b8fcfb80061349e801953c9812a6a5f1816c
ByeongTaek <respect21047436@gmail.com>
2023-12-18 15:42:50 +0900

    env fixed

--- deep-work/src/api/todoList.tsx
+++ deep-work/src/api/todoList.tsx
@@ -3,22 +3,22 @@ import { Addto } from "../components/Input";
 
 // 모든 todos를 가져오는 api
 const getTodos = async ():Promise<Addto[]> => {
-  const response:AxiosResponse<Addto[]> = await axios.get("http://localhost:3001/todos");
+  const response:AxiosResponse<Addto[]> = await axios.get(`${process.env.REACT_APP_SERVER_URL}`);
 
   return response.data;
 
 };
 
 const addTodo = async (newTodo:Addto) => {
-  await axios.post("http://localhost:3001/todos", newTodo);
+  await axios.post(`${process.env.REACT_APP_SERVER_URL}`, newTodo);
 };
 
 const deleteTodo = async (id:string) => {
-  await axios.delete(`http://localhost:3001/todos/${id}`)
+  await axios.delete(`${process.env.REACT_APP_SERVER_URL}/${id}`)
 } 
 
 const switchTodo = async (item:Addto) => {
-  await axios.patch(`http://localhost:3001/todos/${item.id}`, { isDone: !item.isDone });
+  await axios.patch(`${process.env.REACT_APP_SERVER_URL}/${item.id}`, { isDone: !item.isDone });
 } 
 
 export { addTodo,getTodos,deleteTodo,switchTodo };
