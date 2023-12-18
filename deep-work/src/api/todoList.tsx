1055ee4587a06b9db2c98920449913aeef09ec92
ByeongTaek <respect21047436@gmail.com>
2023-12-18 16:13:45 +0900

    repush

--- deep-work/src/api/todoList.tsx
+++ deep-work/src/api/todoList.tsx
@@ -3,22 +3,22 @@ import { Addto } from "../components/Input";
 
 // 모든 todos를 가져오는 api!!
 const getTodos = async ():Promise<Addto[]> => {
-  const response:AxiosResponse<Addto[]> = await axios.get(`${process.env.REACT_APP_SERVER_URL}`);
+  const response:AxiosResponse<Addto[]> = await axios.get(`${process.env.REACT_APP_SERVER_URL}/todos`);
 
   return response.data;
 
 };
 
 const addTodo = async (newTodo:Addto) => {
-  await axios.post(`${process.env.REACT_APP_SERVER_URL}`, newTodo);
+  await axios.post(`${process.env.REACT_APP_SERVER_URL}/todos`, newTodo);
 };
 
 const deleteTodo = async (id:string) => {
-  await axios.delete(`${process.env.REACT_APP_SERVER_URL}/${id}`)
+  await axios.delete(`${process.env.REACT_APP_SERVER_URL}/todos/${id}`)
 } 
 
 const switchTodo = async (item:Addto) => {
-  await axios.patch(`${process.env.REACT_APP_SERVER_URL}/${item.id}`, { isDone: !item.isDone });
+  await axios.patch(`${process.env.REACT_APP_SERVER_URL}/todos/${item.id}`, { isDone: !item.isDone });
 } 
 
 export { addTodo,getTodos,deleteTodo,switchTodo };
